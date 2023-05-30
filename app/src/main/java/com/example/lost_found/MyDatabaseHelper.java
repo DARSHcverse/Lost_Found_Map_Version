bb673d39a33f43c59177df30078ff0a02e26abae
Darshan <109759629+DARSHcverse@users.noreply.github.com>
2023-05-30 19:00:25 +1000

    Commited

--- app/src/main/java/com/example/lost_found/MyDatabaseHelper.java
+++ app/src/main/java/com/example/lost_found/MyDatabaseHelper.java
@@ -7,6 +7,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String MyDatabase="mydatabase.db";
@@ -32,4 +35,23 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         Cursor cursor=db.rawQuery("SELECT * FROM MyTable",null);
         return cursor;
     }
+    public List<String> getColumnValues(String tableName, String columnName) {
+        List<String> columnValues = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(tableName, new String[]{columnName}, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String value = cursor.getString(cursor.getColumnIndex(columnName));
+                columnValues.add(value);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return columnValues;
+    }
+
 }
